fba083e4a3df49624d27e37ead20049b1cf3f601
Thomas Frössman <thomasf@jossystem.se>
2013-04-30 07:21:25 +0200

    no window menu, adjust mouse bindings.

--- .xmonad/lib/XMonad/Config/A00001.hs
+++ .xmonad/lib/XMonad/Config/A00001.hs
@@ -238,9 +238,7 @@ myKeys conf =
     getSortByTagNoSP = fmap (.namedScratchpadFilterOutWorkspace) getSortByTag
 
     -- | Restore focused window from minimized state
-    restoreFocused = do
-      withFocused $ \w -> do
-        sendMessage (RestoreMinimizedWin w)
+    restoreFocused = withFocused $ \w -> sendMessage (RestoreMinimizedWin w)
 
 
 -- | Mouse bindings
@@ -250,12 +248,14 @@ myMouseBindings =
       button10 :: Button
       button10 =  10
       gestures = M.fromList
-                 [ ([], \w -> focus w >> windowMenu)
+                 [ ([    ], \w -> focus w)
                  , ([R, D], \_ -> sendMessage NextLayout >> showLayoutName)
-                 , ([U], \w -> focus w >> Nav2d.windowSwap U False)
-                 , ([D], \w -> focus w >> Nav2d.windowSwap D False)
-                 , ([L], \w -> focus w >> Nav2d.windowSwap L False)
-                 , ([R], \w -> focus w >> Nav2d.windowSwap R False)
+                 , ([L, U], \w -> sendMessage RestoreNextMinimizedWin >> focus w)
+                 , ([L, D], \w -> focus w >> withFocused minimizeWindow)
+                 , ([U   ], \w -> focus w >> Nav2d.windowSwap U False)
+                 , ([D   ], \w -> focus w >> Nav2d.windowSwap D False)
+                 , ([L   ], \w -> focus w >> Nav2d.windowSwap L False)
+                 , ([R   ], \w -> focus w >> Nav2d.windowSwap R False)
                  ]
 
 
@@ -516,40 +516,6 @@ workspaceAction = do
 maybeWorkspaceAction = do
   wins <- gets (W.integrate' . W.stack . W.workspace . W.current . windowset)
   when (null wins) workspaceAction
-
-windowMenu :: X ()
-windowMenu = withFocused $ \w -> do
-    Rectangle x y wh ht <- getSize w
-    Rectangle sx sy swh sht <- gets $ screenRect . W.screenDetail . W.current . windowset
-    let originFractX = (fi x - fi sx + fi wh / 2) / fi swh
-        originFractY = (fi y - fi sy + fi ht / 2) / fi sht
-        gsConfig = (buildDefaultGSConfig colorizer)
-                    { gs_originFractX = originFractX
-                    , gs_originFractY = originFractY }
-        actions = [ ("Cancel menu", return ())
-                  , ("Close"      , kill)
-                  , ("Restore minimized window" , sendMessage RestoreNextMinimizedWin)
-                  , ("Minimize"   , minimizeWindow w)
-                  ]
-    runSelectedAction gsConfig actions
-      where
-    colorizer :: a -> Bool -> X (String, String)
-    colorizer _ isFg = do
-        fBC <- asks (focusedBorderColor . config)
-        nBC <- asks (normalBorderColor . config)
-        return $ if isFg
-                    then (fBC, nBC)
-                    else (nBC, fBC)
-
-    getSize :: Window -> X (Rectangle)
-    getSize w = do
-      d  <- asks display
-      wa <- io $ getWindowAttributes d w
-      let x = fi $ wa_x wa
-          y = fi $ wa_y wa
-          wh = fi $ wa_width wa
-          ht = fi $ wa_height wa
-      return (Rectangle x y wh ht)
 
 
 -- Local Variables:
