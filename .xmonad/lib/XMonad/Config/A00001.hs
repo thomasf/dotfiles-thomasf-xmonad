c55ce03696a15f37328771150ac61a8d5542c75f
Thomas Frössman <thomasf@jossystem.se>
2013-10-02 17:57:32 +0200

    larger

--- .xmonad/lib/XMonad/Config/A00001.hs
+++ .xmonad/lib/XMonad/Config/A00001.hs
@@ -408,7 +408,7 @@ myManageHook =
   , [title     =? "Onboard"  -?>                               doFloat]
   ]) <+> manageDocks
   where
-    doCenterFloatLarge = myCenterFloat 0.7 0.85
+    doCenterFloatLarge = myCenterFloat 0.9 0.85
     doSink = ask >>= doF . W.sink
     role = stringProperty "WM_WINDOW_ROLE"
     startsWith' :: Eq a => Query [a] -> [a] -> Query Bool
